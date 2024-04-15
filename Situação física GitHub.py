5fc9f382f3c32bc3da21619192383210e209833c
CauanCambi <167104394+CauanCambi@users.noreply.github.com>
2024-04-15 13:51:02 -0300

    Add files via upload

---
+++ Situação física GitHub.py
@@ -0,0 +1,9 @@
+velocidadeMetrosPorSeg = 10  
+
+velocidadeKmPorH = velocidadeMetrosPorSeg * 3.6  
+
+TempoSemanaHoras = 7 * 24 
+
+distancia_KM = velocidadeKmPorH * TempoSemanaHoras
+
+print("A distância percorrida é de aproximadamente", distancia_KM, "Km/h:")
